2201ca4a1de59a17cf80ddd53b1e8ac622443ed9
Par9uet <1607611087@qq.com>
2024-10-14 15:40:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ConfigProvider, notification } from "ant-design-vue";
+import { onMounted } from "vue";
+
+const [api, ContextHolder] = notification.useNotification({
+  top: "80px",
+});
+
+onMounted(() => {
+  api.info({
+    message: "test",
+    description: "test",
+    duration: 0,
+  });
+});
+</script>
+
+<template>
+  <ConfigProvider>
+    App
+    <context-holder />
+  </ConfigProvider>
+</template>
+
+<style scoped></style>
